c4b0e31cfbd32d809469024d053641ebdded2922
kiey <joan.marcual.medina@gmail.com>
2020-08-03 11:18:28 +0200

    Unit tests added and fuctional tests, functional tests now check we can input a to do list
    D

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,4 +1,5 @@
-"""superlists URL Configuration
+"""
+superlists URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.urls import include, re_path
+from lists import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('', views.home_page, name='home'),
 ]
