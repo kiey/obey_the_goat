54a759021998c4f937aaa3293750fc93351586e0
kiey <joan.marcual.medina@gmail.com>
2020-08-04 10:29:19 +0200

    created test to check lists

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+import re
 from django.template.loader import render_to_string
 
 
@@ -8,13 +9,39 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
+
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        html = self.remove_csrf(response.content.decode())
+
+        self.assertEqual(html, expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        html = self.remove_csrf(response.content.decode())
+
+        self.assertIn('A new list item', html)
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(html, expected_html)
+
+
 
