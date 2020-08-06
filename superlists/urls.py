0f95097b9716ab576ff39007a9eb15ed58d6b452
kiey <joan.marcual.medina@gmail.com>
2020-08-06 19:14:47 +0200

    minimum app working

--- superlists/urls.py
+++ superlists/urls.py
@@ -20,9 +20,9 @@ from django.urls import include, re_path
 from lists import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.home_page, name='home'),
-    path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
-    path('lists/new', views.new_list, name='new_list'),
-
+    re_path(r'^$', views.home_page),
+    path('lists/<int:list_id>/add_item', views.add_item),
+    path('lists/<int:list_id>/', views.view_list),
+    re_path(r'^lists/new$', views.new_list),
 
 ]
